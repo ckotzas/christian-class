288c96a9c950b1657dd4bfb8f8e8533bce09b6ce
ckotzas <ckotzas@gmail.com>
2013-03-13 23:36:07 -0400

    Homework 14

---
+++ Class 14/Workshop/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+	$('button').click(function () {
+		$('#ball').animate({
+			bottom: '+=300'
+		})
+
+	});
+
+
+});
+$(document).ready(function() {
+
+	$('button').click(function () {
+		$('#ball').animate({
+			left: '+=360'
+		})
+		
+	});
+
+
+});
+$(document).ready(function() {
+
+	$('button').click(function () {
+		$('#ball').animate({
+			bottom: '-=300'
+		})
+		
+	});
+
+
+});
+$(document).ready(function() {
+
+	$('button').click(function () {
+		$('#ball').animate({
+			left: '-=360'
+		})
+		
+	});
+
+
+});
